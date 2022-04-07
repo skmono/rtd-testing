5726e351cbb600eb4e47ab6640da07fe3d80a4b3
sejunkim <sejun.kim@intel.com>
2022-04-06 17:12:35 -0700

    teswt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ipcl-python'
+copyright = '2022, Intel'
+author = 'Sejun Kim'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
